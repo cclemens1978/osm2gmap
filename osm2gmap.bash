cf4b5d059173a501414f5425f9a8df44faacf31e
Christian Clemens <christian.clemens@web.de>
2020-08-12 16:58:49 +0200

    enhanced osm2gmap.bash

--- osm2gmap.bash
+++ osm2gmap.bash
@@ -16,30 +16,41 @@ done
 cd mkgmap/
 
 #
-## download and process map (Germany)
+## download and process map of Germany
 
 until test -f gmapsupp.img; do
 
   # clean up working directory
-  rm -vf densities-out.txt areas.* template.args problems.list 6324*.o5m 6324*.img ovm_6324*.img osmmap.*
+  rm --verbose --force densities-out.txt areas.* template.args problems.list 6324*.o5m 6324*.img ovm_6324*.img osmmap.*
 
-  # date
+  # query date
   IDX=$(date +%Y-%m-%d)
 
-  # wget and osmconvert
+  # create map directory
   mkdir --parents ../maps/
-  wget --tries=0 --output-document=- http://download.geofabrik.de/europe/germany-latest.osm.pbf | \
-    ../osmconvert/osmconvert - --out-osm | \
-    bzip2 --best > "../maps/germany_${IDX}.osm.bz2"
 
-  # splitter.jar
+  # uncomment these two lines and comment the following three lines if you want to use a map downloaded in advance, e.g. from the internal password-protected area of the Geofabrik GmbH
+
+  # # use a map of Germany downloaded in advance
+  # TMPFILE="/tmp/germany-latest-internal.osm.pbf"
+
+  # download current map of Germany
+  TMPFILE="$(mktemp --tmpdir=/dev/shm/)"
+  wget --output-document="${TMPFILE}" https://download.geofabrik.de/europe/germany-latest.osm.pbf
+
+  # execute osmconvert and bzip2
+  ../osmconvert/osmconvert --verbose "${TMPFILE}" --out-osm | \
+  bzip2 --best > "../maps/germany_${IDX}.osm.bz2"
+  rm --verbose --force "${TMPFILE}"
+
+  # execute splitter.jar
   java -Xmx2048M -jar splitter.jar \
     --problem-report=problems.list \
     --write-kml=areas.kml \
     --output=o5m \
     "../maps/germany_${IDX}.osm.bz2"
 
-  # mkgmap.jar
+  # execute mkgmap.jar
   java -Xmx2048M -jar mkgmap.jar \
     --country-name=GERMANY \
     --country-abbr=D \
@@ -49,6 +60,6 @@ until test -f gmapsupp.img; do
     --read-config=template.args
 
   # clean up working directory
-  rm -vf 6324*.o5m 6324*.img ovm_6324*.img osmmap.*
+  rm --verbose --force 6324*.o5m 6324*.img ovm_6324*.img osmmap.*
 
 done
